8810cac2a9fc64e8fc85f67e1c22ef8d7234215b
Mateusz Wronski <ustka666@poczta.onet.pl>
2019-02-05 11:48:07 +0100

    8: done

---
+++ 8/src/main/kotlin/Strings.kt
@@ -0,0 +1,39 @@
+import com.google.common.io.Resources
+import java.io.File
+
+fun main() {
+    fun8()
+}
+
+fun fun8() {
+    val lines = File(Resources.getResource("input.txt").toURI()).readLines()
+    val codeSize = lines.map(String::length).sum()
+    lines.map(String::transformToMem).forEach(::println)
+    val memSize = lines.map(String::transformToMem).map { it.length }.sum()
+
+
+    println("Code size: $codeSize, mem size: $memSize diff: ${codeSize - memSize}")
+
+    val escapedSize = lines.map(String::escape).map(String::length).sum()
+
+    println("Code size: $codeSize, escaped size: $escapedSize diff: ${escapedSize - codeSize}")
+}
+
+fun String.transformToMem(): String {
+    return this
+        .removePrefix("\"")
+        .removeSuffix("\"")
+        .replace("\\\\", "\\")
+        .replace("\\\"", "\"")
+        .replace("\\\\x[0-9a-f]{2}".toRegex(), "R")
+}
+
+fun String.escape(): String {
+    return this.map {
+        when (it) {
+            '\"' -> "\\\""
+            '\\' -> "\\\\"
+            else -> it.toString()
+        }
+    }.joinToString(separator = "", prefix = "\"", postfix = "\"")
+}
---
+++ 8/src/test/kotlin/StringsKtTest.kt
@@ -0,0 +1,40 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class StringsKtTest {
+
+    @Test
+    fun `should strip trailing and leading quote`() {
+        assertEquals("a", "\"a\"".transformToMem());
+    }
+
+    @Test
+    fun `should replace escaped quote`() {
+        assertEquals("a\"a", "a\\\"a".transformToMem());
+    }
+
+    @Test
+    fun `should replace escaped backslash`() {
+        assertEquals("a\\", "a\\\\".transformToMem());
+    }
+
+    @Test
+    fun `should replace ascii code with letter R`() {
+        assertEquals("aR", "a\\xff".transformToMem());
+    }
+
+    @Test
+    fun `should escape quotes and quote`() {
+        assertEquals("\"\\\"\\\"\"", "\"\"".escape());
+    }
+
+    @Test
+    fun `should quote`() {
+        assertEquals("\"\\\"abc\\\"\"", "\"abc\"".escape());
+    }
+
+    @Test
+    fun `should quote and escape slashes and quotes`() {
+        assertEquals("\"\\\"aaa\\\\\\\"aaa\\\"\"", "\"aaa\\\"aaa\"".escape());
+    }
+}
