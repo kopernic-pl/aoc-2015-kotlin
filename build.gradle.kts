fa1072c06439677882b2038c103e3c827110fd25
Mateusz Wronski <mateusz.wronski@smartbear.com>
2021-11-09 20:49:27 +0100

    version of jvm tuned

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ subprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "17"
+            jvmTarget = "16"
         }
     }
 
